0bf477b10e7ca2cb055513401c7115d300ce969a
Prashanth Rajaram <prashanthr@users.noreply.github.com>
2020-10-26 18:31:07 -0700

    fix(webpack-svg): Fix svg build (#16)
    Co-authored-by: Prashanth R <1409501+prashanthr@users.noreply.github.com>

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,17 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.svg$/i,
+        use: [
+          {
+            loader: 'svg-url-loader',
+            options: {
+              limit: 10000,
+            },
+          },
+        ],
+      }
     ]
   }
 }
